44da61715cce7f642387464ee09ba723cac75730
Enrico Lombardo <enricoflombardo5@gmail.com>
2022-09-05 20:45:29 +0700

    commit rico

--- tests/data/booking.data.js
+++ tests/data/booking.data.js
@@ -9,3 +9,15 @@ export const VALID_BOOKING = {
     },
     "additionalneeds" : "Breakfast"
 }
+
+export const VALID_UPDATE = {
+    "firstname" : "James Bond",
+    "lastname" : "Brown",
+    "totalprice" : 2000,
+    "depositpaid" : true,
+    "bookingdates" : {
+        "checkin" : "2018-01-01",
+        "checkout" : "2019-01-01"
+    },
+    "additionalneeds" : "Breakfast"
+}
--- tests/pages/herokuapp.api.js
+++ tests/pages/herokuapp.api.js
@@ -14,6 +14,12 @@ const herokuappAPI = {
     booking_get_fullname: () => BaseAPI.get('/booking?firstname=sally&lastname=brown'),
     booking_get_date: () => BaseAPI.get('/booking?checkin=2014-03-13&checkout=2014-05-21'),
     booking_get_invalid: () => BaseAPI.get('/booking/98112'),
+
+    update_booking: (data, token) => BaseAPI.patch('/booking/21', data, {
+        headers: {
+            'cookie': 'token='+token
+        }
+    })
     
 }
 
